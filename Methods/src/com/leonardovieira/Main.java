b504fbc87325e55b296c9691890892cff09df668
LeoVieira1997 <32408333+LeoVieira1997@users.noreply.github.com>
2018-12-05 12:34:26 -0200

    Lecture 29

--- Methods/src/com/leonardovieira/Main.java
+++ Methods/src/com/leonardovieira/Main.java
@@ -1,24 +1,47 @@
 package com.leonardovieira;
 
+import org.jetbrains.annotations.Contract;
+
 public class Main {
 
     public static void main(String[] args) {
-        calculateScore();
+        /*int highScore = calculateScore(true, 1000, 5, 100);
+        System.out.println(highScore);*/
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Leonardo", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Percy", highScorePosition);
     }
 
-    public static void calculateScore(){
-        boolean gameOver = true;
-        int score = 3000;
-        int levelCompleted = 5;
-        int bonus = 100;
-
+    @Contract(pure = true)
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-            score = 10000;
-            levelCompleted = 8;
-            bonus = 200;
-            System.out.println("Your second final score was " + (finalScore = score + (levelCompleted * bonus)));
+            return finalScore;
+        }
+        return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position){
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if (playerScore >= 1000){
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000){
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500){
+            return 3;
+        } else {
+            return 4;
         }
     }
 }
