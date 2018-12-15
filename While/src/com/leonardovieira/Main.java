2b4ebc344a904b93630406e1b230d4242ed6ba18
LeoVieira1997 <32408333+LeoVieira1997@users.noreply.github.com>
2018-12-15 17:08:23 -0200

    More on Lecture 45

--- While/src/com/leonardovieira/Main.java
+++ While/src/com/leonardovieira/Main.java
@@ -3,6 +3,30 @@ package com.leonardovieira;
 public class Main {
 
     public static void main(String[] args) {
+        int number = 3;
+        int finishNumber = 20;
+        int count = 0;
+        int sum = 0;
+        while((number <= finishNumber) && (count < 5)){
+            if (!isEvenNumber(number)){
+//                System.out.println(number + " isn't even");
+                number++;
+                continue;
+            }
+            System.out.println(number + " is even");
+            sum += number;
+            number++;
+            count++;
+        }
+        System.out.println(sum);
 
     }
+
+    public static boolean isEvenNumber(int number){
+        if ((number % 2) == 0){
+            return true;
+        }
+        return false;
+    }
+
 }
