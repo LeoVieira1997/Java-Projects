383b6acb39ea33ac3bd6f312049b00ecf9cdc662
LeoVieira1997 <32408333+LeoVieira1997@users.noreply.github.com>
2018-12-19 23:31:09 -0200

    Lecture 49

---
+++ ReadingUserInput/src/com/leonardovieira/Main.java
@@ -0,0 +1,19 @@
+package com.leonardovieira;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        int age = 2018 - yearOfBirth;
+        scanner.nextLine();
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is " + name + ", and you're " + age + " years old");
+        scanner.close();
+    }
+}
